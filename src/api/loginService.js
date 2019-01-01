43358a32552651c2e26df2a65947bd5e1d758316
Irshad Arippayil <irshadpalayadan@gmail.com>
2019-01-01 23:21:59 +0530

    base url setting

--- src/api/loginService.js
+++ src/api/loginService.js
@@ -1,20 +1,21 @@
+const baseUrl = 'http://localhost:5000';
 const loginService = {
     signin : function(username, password, callback) {
-        return fetch("http://localhost:5000/api/auth/signin?user=" + username + "&pass=" + password,{method: 'GET', credentials:'include'})
+        return fetch( baseUrl + "/api/auth/signin?user=" + username + "&pass=" + password,{method: 'GET', credentials:'include'})
     },
     signup: function(username, email, phno, password, callback){
-        return fetch("http://localhost:5000/api/auth/signup?user="+username+"&email="+email+"&phno="+phno+"&pass="+password,{method: 'POST', credentials:'include'})
+        return fetch( baseUrl + "/api/auth/signup?user="+username+"&email="+email+"&phno="+phno+"&pass="+password,{method: 'POST', credentials:'include'})
     },
     checkSignInStatus: function() {
-        return fetch('http://localhost:5000/api/auth/signinstatus', {method: 'GET', credentials:'include'});
+        return fetch( baseUrl + '/api/auth/signinstatus', {method: 'GET', credentials:'include'});
     },
     getUserProfile: function() {
-        return fetch('http://localhost:5000/api/auth/userprofile', {method: 'GET', credentials:'include'});
+        return fetch( baseUrl + '/api/auth/userprofile', {method: 'GET', credentials:'include'});
     },
     signout: function() {
-        return fetch('http://localhost:5000/api/auth/signout', {method: 'GET', credentials:'include'});
+        return fetch( baseUrl + '/api/auth/signout', {method: 'GET', credentials:'include'});
     },
-    google: 'http://localhost:5000/api/auth/google',
+    google:  baseUrl + '/api/auth/google',
 }
 
 export default loginService;
